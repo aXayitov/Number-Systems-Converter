378511d7e77bf35c3660c7b054905483fa33eef1
aXayitov <hayitovabdurahmon76@gmail.com>
2024-11-24 23:51:57 +0500

    10 => 16

--- ForCloud/Program.cs
+++ ForCloud/Program.cs
@@ -50,3 +50,27 @@
 
 //    return string.IsNullOrEmpty(octal) ? "0" : octal;
 //}
+
+
+// 10 => 16
+
+Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+
+string hexadecimalNumber = ConvertToHexadecimal(decimalNumber);
+Console.WriteLine($"16-lik sanoq sistemasidagi qiymat: {hexadecimalNumber}");
+
+static string ConvertToHexadecimal(int decimalNumber)
+{
+    string hexChars = "0123456789ABCDEF";
+    string hex = string.Empty;
+
+    while (decimalNumber > 0)
+    {
+        int remainder = decimalNumber % 16;
+        hex = hexChars[remainder] + hex;
+        decimalNumber /= 16;
+    }
+
+    return string.IsNullOrEmpty(hex) ? "0" : hex;
+}
--- ForCloud/obj/Debug/net8.0/ForCloud.AssemblyInfo.cs
+++ ForCloud/obj/Debug/net8.0/ForCloud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97add317b84296f2619d9daa73001eaa8fc6b35c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
