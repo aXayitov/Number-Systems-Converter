97add317b84296f2619d9daa73001eaa8fc6b35c
aXayitov <hayitovabdurahmon76@gmail.com>
2024-11-24 23:47:42 +0500

    10 likdan 8 likga o'tkazuvchi dastur qoshildi

--- ForCloud/Program.cs
+++ ForCloud/Program.cs
@@ -27,3 +27,26 @@
 //}
 
 
+
+// 10 => 8
+
+//Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+//int decimalNumber = int.Parse(Console.ReadLine());
+
+//string octalNumber = ConvertToOctal(decimalNumber);
+//Console.WriteLine($"8-lik sanoq sistemasidagi qiymat: {octalNumber}");
+
+
+//static string ConvertToOctal(int decimalNumber)
+//{
+//    string octal = string.Empty;
+
+//    while (decimalNumber > 0)
+//    {
+//        int remainder = decimalNumber % 8;
+//        octal = remainder + octal;
+//        decimalNumber /= 8;
+//    }
+
+//    return string.IsNullOrEmpty(octal) ? "0" : octal;
+//}
