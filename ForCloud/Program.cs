fd2d5751d63dc1a1a95695d1217834ad2da215ac
aXayitov <hayitovabdurahmon76@gmail.com>
2024-11-24 23:44:48 +0500

    10 lik sanoq sistemasidagi sonni 2 lik sanoq sistemasiga o'tkazuvchi dastur

---
+++ ForCloud/Program.cs
@@ -0,0 +1,29 @@
+﻿
+/// 10 => 2
+
+//Console.Write("10-lik sanoq sistemasidagi sonni kiriting: ");
+//int number = int.Parse(Console.ReadLine());
+
+//string binary = ConvertToBinary(number);
+
+//Console.WriteLine($"2-lik sanoq sistemasida: {binary}");
+
+
+//static string ConvertToBinary(int number)
+//{
+//    if (number == 0)
+//        return "0";
+
+//    string binary = "";
+
+//    while (number > 0)
+//    {
+//        int remainder = number % 2;
+//        binary = remainder + binary;
+//        number /= 2;
+//    }
+
+//    return binary;
+//}
+
+
